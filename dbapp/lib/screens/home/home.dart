fb9444f995a6cdbe9ea35bb68d738a260d3468bc
Nitasha Dhingra <44186440+nitashadhingra@users.noreply.github.com>
2020-05-17 11:43:05 +0530

    Update home.dart

--- dbapp/lib/screens/home/home.dart
+++ dbapp/lib/screens/home/home.dart
@@ -1,110 +1,57 @@
 import 'package:dbapp/services/auth.dart';
 import 'package:flutter/material.dart';
-import 'package:dbapp/screens/profile/profile.dart';
 import 'package:dbapp/screens/sidebarScreens/page.dart';
+import 'package:dbapp/screens/profile/profile.dart';
+import 'package:dbapp/screens/chat/chat.dart';
+
+import 'package:dbapp/screens/home/homepage.dart';
+
 
 class Home extends StatefulWidget {
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex=0;
   static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
+  final List<Widget> _children = <Widget>[
+    HomePage(),
     profile(),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
+    FriendlyChatApp()
   ];
-  int _selectedIndex = 0;
 
   void _onItemTapped(int index) {
     setState(() {
-      _selectedIndex = index;
+      _currentIndex = index;
     });
   }
 
-  final AuthService _auth=AuthService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title:Text("myapp"),
-        backgroundColor:Colors.brown[400] ,
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: () async{
-              await _auth.signOut();
-            }, 
-            icon: Icon(Icons.person),
-            label:Text('logout')
-            )
-        ],
-      ),
-      drawer: new Drawer(
-        child: new ListView(
-          children: <Widget>[
-            new ListTile(
-              title: new Text("Page One"),
-              trailing: new Icon(Icons.arrow_upward),
-              onTap: () {
-                Navigator.of(context).pop();
-               // Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new Page("First Page")));
-              }
-            ),
-            new ListTile(
-              title: new Text("Page Two"),
-              trailing: new Icon(Icons.arrow_right),
-              onTap: () {
-                Navigator.of(context).pop();
-                //Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new Page("Second Page")));
-              }
-            ),
-            new Divider(),
-            new ListTile(
-              title: new Text("Cancel"),
-              trailing: new Icon(Icons.cancel),
-              onTap: () => Navigator.pop(context),
-            ),
-          ],
-        ),
-      ),
-    
-    
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
+      body:_children[_currentIndex],
     bottomNavigationBar: BottomNavigationBar(
       type: BottomNavigationBarType.fixed,
       items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.account_circle),
-          title: Text('Profile'),
-        ),
         BottomNavigationBarItem(
           icon: Icon(Icons.home),
           title: Text('Home'),
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.chat_bubble),
+          icon: Icon(Icons.person),
+          title: Text('Profile'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.chat),
           title: Text('Chat'),
         ),
       ],
-      currentIndex: _selectedIndex,
+      currentIndex: _currentIndex,
       selectedItemColor: Colors.amber[800],
       onTap: _onItemTapped,
     ),
     );
   }
 }
-
-
