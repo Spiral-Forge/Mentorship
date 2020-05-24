42d1301dbcfdca47c165a72f9444d01a44b5f4a4
Suhani Chawla <suhanichawla2000@gmail.com>
2020-05-24 16:26:53 +0530

    Guildelines page

--- lib/screens/sidebarScreens/guidelines.dart
+++ lib/screens/sidebarScreens/guidelines.dart
@@ -6,9 +6,29 @@ class Guidelines extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("Guidelines"), backgroundColor: Colors.redAccent),
-      body: new Center(
-        child: Text("Code of conduct for mentors and mentees"),
-      ),
+      body: Container(
+        padding: EdgeInsets.only(top:20.0),
+        child: Center(
+          child: Column(
+            children:<Widget>[
+              Text("Heading"),
+              SizedBox(height:20.0),
+              Text("Guideline 1"),
+              SizedBox(height:10.0),
+              Text("Guideline 2"),
+              SizedBox(height:10.0),
+              Text("Guideline 3"),
+              SizedBox(height:10.0),
+              Text("Guideline 4"),
+              SizedBox(height:10.0),
+              Text("Guideline 5"),
+            ]
+          ),
+        ),
+      )
+      // new Center(
+      //   child: Text("Code of conduct for mentors and mentees"),
+      // ),
     );
   }
 }
