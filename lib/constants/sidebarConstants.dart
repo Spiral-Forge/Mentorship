e51c7a2a3e9e2e79c4d4a8bf12bf76a4be5b8e3a
Urvi Goel <urvigoel26@gmail.com>
2020-08-18 01:02:18 +0530

    added faqs and guidelines

--- lib/constants/sidebarConstants.dart
+++ lib/constants/sidebarConstants.dart
@@ -1,56 +1,73 @@
-class SidebarConstants{
-  static final List<String> guidelines=[
-    "Lorem Ipsum is simply dummy text of the printin.Lorem Ipsum is simply dummy text of the printin.Lorem Ipsum is simply dummy text of the printin.Lorem Ipsum is simply dummy text of the printin.Lorem Ipsum is simply dummy text of the printin.Lorem Ipsum is simply dummy text of the printin.Lorem Ipsum is simply dummy text of the printin.Lorem Ipsum is simply dummy text of the printin.",
-    "Guidelinemm",
-    "Guideline3",
-    "Guideline4",
-    "guideline5S",
-    "Guideline2",
-    "Guideline3",
-    "Guideline4",
-    "guideline5S",
-    "Guideline2",
-    "Guideline3",
-    "Guideline4",
-    "guideline5S",
-    "Guideline2",
-    "Guideline3",];
+class SidebarConstants {
+  static final List<String> guidelinesMentors = [
+    "You can see the profile page of your mentees in the ‘Know your mentees section’ on the homepage.",
+    "As soon as you’re connected to your mentees, introduce yourself, share your contact and get familiar to each other.",
+    "Try to build a friendly rather than a professional connection with your mentees from day 1 so that they feel comfortable addressing their doubts to you.",
+    "Let the mentees know how you would like to be contacted, how often and what are your preferred times.",
+    "Ask your mentees what they want to achieve and make short-term and long-term plans with them.",
+    "If your mentees need help in a domain that you don’t have experience in, help them connect with your batchmates/friends with relevant experience. If that is not possible, reach out to the core team of Protégé for help.",
+    "While adding links to resources in the resource center, make sure you add them with a suitable title and under the right category. Any mentor found misusing the resource center will face serious repercussions.",
+    "Remind your mentees about any upcoming scholarship, internship, or other opportunities and motivate them to apply for the same.",
+    "For any feedback/suggestions/complaints about the app, fill out the feedback form.",
+    "If you find your mentee not showing considerable improvement or lacking efforts, give her two warnings, after which the issue should be reported to the core team members of Protégé.",
+    "Mentees",
+    "You can see the profile page of your mentor in the ‘Know your mentor section’ on the homepage.",
+    "As soon as you’re connected to your mentor, introduce yourself, share your contact and get familiar to each other.",
+    "Ask your mentor about her preferred medium and preferred time to connect with her. Be respectful towards your mentor’s time.",
+    "Although we encourage mentors and mentees to have a friendly rather than a professional relationship, make sure to not overstep the boundaries (by messaging too often, calling during inappropriate time of the day, etc.)",
+    "Feel free to address any kind of questions to your mentor. At the same time, keep in mind that your mentor may not have experience in all domains and she might need time to figure out how to help you in the best way possible.",
+    "Let your mentor know what you want to achieve and make short-term and long-term plans with her.",
+    "Before asking for any resource from your mentor, check the resource center as you might find almost everything that you need there.",
+    "Keep a regular check on the home for upcoming events. If you’re not sure whether you should attend an event, ask your mentor about it.",
+    "For any feedback/suggestions/complaints about the app, fill out the feedback form.",
+    "If you feel your mentor is not giving you satisfactory experience or is unable to give you appropriate time, send a feedback or contact the core team of Protégé directly.",
+  ];
 
+  static final List<String> guidelinesMentees = [
+    "You can see the profile page of your mentor in the ‘Know your mentor section’ on the homepage.",
+    "As soon as you’re connected to your mentor, introduce yourself, share your contact and get familiar to each other.",
+    "Ask your mentor about her preferred medium and preferred time to connect with her. Be respectful towards your mentor’s time.",
+    "Although we encourage mentors and mentees to have a friendly rather than a professional relationship, make sure to not overstep the boundaries (by messaging too often, calling during inappropriate time of the day, etc.)",
+    "Feel free to address any kind of questions to your mentor. At the same time, keep in mind that your mentor may not have experience in all domains and she might need time to figure out how to help you in the best way possible.",
+    "Let your mentor know what you want to achieve and make short-term and long-term plans with her.",
+    "Before asking for any resource from your mentor, check the resource center as you might find almost everything that you need there.",
+    "Keep a regular check on the home for upcoming events. If you’re not sure whether you should attend an event, ask your mentor about it.",
+    "For any feedback/suggestions/complaints about the app, fill out the feedback form.",
+    "If you feel your mentor is not giving you satisfactory experience or is unable to give you appropriate time, send a feedback or contact the core team of Protégé directly.",
+  ];
 
-   static final List<String> faqQuestionAnswers=[
-    "fffffffffffffffff",
-   "aLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 1",
-   "q2",
-   "a2",
-   "q3",
-   "ans3",
-   "q4",
-   "q5",
-   "1",
-   "2",
-   "q1",
-   "a1",
-   "q2",
-   "a2",
-   "q3",
-   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
-   ];
+  static final List<String> faqQuestionAnswers = [
+    "When will I be connected with my mentor/mentee?",
+    "We take some time to study all profiles and match mentees with a mentor who is right for them. So be patient and you will be notified as soon as you’re connected with your mentor/mentee.",
+    "I am facing technical issues with the app. What do I do?",
+    "Send us feedback through the app describing your issue. If that is not possible, reach out to us at spiralforge4@gmail.com.",
+    "I am unable to form a friendly connection with my mentor/mentee and would like to have a change in my mentor/mentee.",
+    "College life is all about bursting your ideal-world-bubble and learning to deal with different kinds of people. Put forth your feelings to your mentor/mentee and try to talk it out. If it still does not work out, contact us at spiralforge4@gmail.com.",
+    "My mentor is not following the guidelines proposed by the society and I wish to report her. Will my identity be shared with her if I do so?",
+    "We assure you that your feedback and identity will be kept confidential in such cases.",
+    "What if I mailed regarding an issue at given email id and hasn’t received a response yet?",
+    "We try to respond to emails within 3-4 working days. If it has been more than 4 days since you emailed us, send us a follow-up email and we will get back to you asap!",
+  ];
 
-  static final List<String> feedbackCCRecipients=['suhanichawla2000@gmail.com'];
-  static final List<String> feedbackRecipients=['suhanichawla2000@gmail.com'];
-  static final Map<int,String> feedbackOptionsMap={
-    1:"Login Problem",
-    2:"Suggestions",
-    3:"Complaints",
-    4:"Other issues",
-    5:"adding new"
+  static final List<String> feedbackCCRecipients = [
+    'suhanichawla2000@gmail.com'
+  ];
+  static final List<String> feedbackRecipients = ['suhanichawla2000@gmail.com'];
+  static final Map<int, String> feedbackOptionsMap = {
+    1: "Login Problem",
+    2: "Suggestions",
+    3: "Complaints",
+    4: "Other issues",
+    5: "adding new"
   };
 
-  static final String aboutSoc='During our first year in IGDTUW, we often felt intimidated by our new surroundings, and found ourselves pondering upon questions like \n\n"Which society should I join?" "Which programming language to learn?" "Which books to refer?" "How to not miss-out on opportunities?" "HOW TO SURVIVE IGDTUW?"\n\nProtégé aims to answer all these questions and more by connecting students with their seniors who will mentor them in all aspects of college life!';
-  static final String founderOshin= '    Oshin Saini \n    oshinsaini18092000@gmail.com' ;
-  static final String founderSuhani='   Suhani Chawla \n   suhanichawla2000@gmail.com ';
-  static final String founderNitasha='   Nitasha Dhingra \n   nitashadhingra612@gmail.com ';
-  static final String founderUrvi='   Urvi Goel \n   urvigoel26@gmail.com';
-   
+  static final String aboutSoc =
+      'During our first year in IGDTUW, we often felt intimidated by our new surroundings, and found ourselves pondering upon questions like \n\n"Which society should I join?" "Which programming language to learn?" "Which books to refer?" "How to not miss-out on opportunities?" "HOW TO SURVIVE IGDTUW?"\n\nProtégé aims to answer all these questions and more by connecting students with their seniors who will mentor them in all aspects of college life!';
+  static final String founderOshin =
+      '    Oshin Saini \n    oshinsaini18092000@gmail.com';
+  static final String founderSuhani =
+      '   Suhani Chawla \n   suhanichawla2000@gmail.com ';
+  static final String founderNitasha =
+      '   Nitasha Dhingra \n   nitashadhingra612@gmail.com ';
+  static final String founderUrvi = '   Urvi Goel \n   urvigoel26@gmail.com';
 }
-
