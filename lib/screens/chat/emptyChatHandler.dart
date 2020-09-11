a0c11452394588e9113276b3c367226485cd84d9
Suhani Chawla <suhanichawla2000@gmail.com>
2020-09-12 04:56:24 +0530

    fixed appbar for empty chat

--- lib/screens/chat/emptyChatHandler.dart
+++ lib/screens/chat/emptyChatHandler.dart
@@ -7,11 +7,43 @@ import 'package:google_fonts/google_fonts.dart';
 class EmptyChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-     final myDrawer _drawer = new myDrawer();
+     final GlobalKey<ScaffoldState> _scaffoldKey =
+        new GlobalKey<ScaffoldState>();
+    final myDrawer _drawer = new myDrawer();
     return Scaffold(
-      appBar: new AppBar(title: new Text("Chat with your mentor"), backgroundColor: AppColors.COLOR_TEAL_LIGHT),
+      // appBar: AppBar(
+      //   title: Text("Chat Room"),
+      //   backgroundColor: AppColors.COLOR_TEAL_LIGHT
+      // ),
       drawer: _drawer,
-      body: Column(
+      key: _scaffoldKey,
+      
+      body:Column(children: [
+        Expanded(
+            child: Container(
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                    child: Column(
+                        // mainAxisAlignment: MainAxisAlignment.center,
+                        // crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(15.0, 42, 0, 0),
+                            child: Row(children: [
+                              IconButton(
+                                  icon: Icon(Icons.menu),
+                                  onPressed: () {
+                                    _scaffoldKey.currentState.openDrawer();
+                                  }),
+                              Text(
+                                "Chat Room",
+                                style: TextStyle(
+                                    fontFamily: 'GoogleSans', fontSize: 23),
+                              )
+                            ]),
+                          ),
+                          Expanded(
+                            child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Center(
@@ -38,6 +70,13 @@ class EmptyChat extends StatelessWidget {
           )
         ],
       )
+                            )
+                           
+                        ])
+                        )
+                        )
+                        )
+        ])
     );
   }
 }
